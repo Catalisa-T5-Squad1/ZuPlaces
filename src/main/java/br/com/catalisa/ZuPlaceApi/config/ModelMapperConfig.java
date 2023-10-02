e522356c0b7db395ff666557bcb4d9bac3747fc0
Guilherme Januário <guilherme.januario@zup.com.br>
2023-10-02 17:13:08 -0300

    :rocket: - Subindo projeto sem erro

--- src/main/java/br/com/catalisa/ZuPlaceApi/config/ModelMapperConfig.java
+++ src/main/java/br/com/catalisa/ZuPlaceApi/config/ModelMapperConfig.java
@@ -8,6 +8,6 @@ import org.springframework.context.annotation.Configuration;
 public class ModelMapperConfig {
     @Bean
     public ModelMapper mapper(){
-        return  new ModelMapper();
+        return new ModelMapper();
     }
 }
