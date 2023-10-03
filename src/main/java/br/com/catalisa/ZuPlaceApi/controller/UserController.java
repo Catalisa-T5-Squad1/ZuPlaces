680a03e9c2c65e5be9659a8cfcea35efd65ca778
AthosDeveloper <athos.silva@zup.com.br>
2023-10-03 15:41:59 -0300

    criando métodos create, update

--- src/main/java/br/com/catalisa/ZuPlaceApi/controller/UserController.java
+++ src/main/java/br/com/catalisa/ZuPlaceApi/controller/UserController.java
@@ -1,15 +1,15 @@
 package br.com.catalisa.ZuPlaceApi.controller;
 
 import br.com.catalisa.ZuPlaceApi.dto.UserDto;
+import br.com.catalisa.ZuPlaceApi.model.UserModel;
 import br.com.catalisa.ZuPlaceApi.service.UserService;
 import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
+import java.net.URI;
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -28,4 +28,19 @@ public class UserController {
                         .map(u -> mapper.map(u, UserDto.class))
                         .collect(Collectors.toList()));
     }
+    @GetMapping(value = ID)
+    public ResponseEntity<UserModel> findById (@PathVariable Long id){
+        UserModel userModel = service.findById(id);
+        return ResponseEntity.ok().body(userModel);
+    }
+    @PostMapping
+    public  ResponseEntity<UserDto> create(UserDto userDto){
+        URI uri = ServletUriComponentsBuilder
+                .fromCurrentRequest()
+                .path(ID)
+                .buildAndExpand(service.create(userDto).getId()).toUri();
+  return ResponseEntity.created(uri).build();
+    }
+
+
 }
