bc82a34cf4d416d3a406198dd0544b9689352174
AthosDeveloper <athos.silva@zup.com.br>
2023-10-04 11:44:44 -0300

    Arrumando atributos e apagando enum

--- src/main/java/br/com/catalisa/ZuPlaceApi/enums/DocumentType.java
+++
@@ -1,5 +0,0 @@
-package br.com.catalisa.ZuPlaceApi.enums;
-
-public enum DocumentType {
-    CPF, CNPJ;
-}
--- src/main/java/br/com/catalisa/ZuPlaceApi/model/UserModel.java
+++ src/main/java/br/com/catalisa/ZuPlaceApi/model/UserModel.java
@@ -1,6 +1,5 @@
 package br.com.catalisa.ZuPlaceApi.model;
 
-import br.com.catalisa.ZuPlaceApi.enums.DocumentType;
 import br.com.catalisa.ZuPlaceApi.enums.PersonType;
 import lombok.AllArgsConstructor;
 import lombok.Data;
@@ -16,19 +15,24 @@ public class UserModel {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
     @Column(nullable = false)
     private String name;
+
     @Column(nullable = false, unique = true)
     private String email;
+
     @Column(nullable = false)
     private String password;
-@Enumerated(EnumType.STRING)
+
+    @Enumerated(EnumType.STRING)
     private PersonType personType;
 
     private String phone;
+
     @Column(nullable = false)
-    @Enumerated(EnumType.STRING)
-private DocumentType documentType;
+    private String documentType;
+
     @Column(nullable = false, unique = true)
     private  String numberAdress;
     //spaces
