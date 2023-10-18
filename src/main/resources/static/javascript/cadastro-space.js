ba8d2e542613ec7d545dad0b14a84048cb1f1837
Guilherme Januário <guilherme.januario@zup.com.br>
2023-10-18 07:47:56 -0300

    Iniciando a tela de cadastro de space no front

--- src/main/resources/static/javascript/cadastro-space.js
+++ src/main/resources/static/javascript/cadastro-space.js
@@ -1,3 +1,39 @@
+function buscarUsers() {
+    fetch('http://localhost:8080/api/users')
+        .then(response => response.json())
+        .then(data => {
+            const escolherUser = document.getElementById('escolher-user');
+
+            escolherUser.innerHTML = '';
+
+            const option = document.createElement('option');
+            option.text = 'Escolha um User';
+            escolherUser.appendChild(option);
+
+            data.forEach(user => {
+                const option = document.createElement('option');
+                option.value = user.id;
+                option.text = user.nome;
+                escolherUser.appendChild(option);
+            });
+        })
+        .catch(error => {
+            console.error(error);
+        });
+}
+
+buscarUsers();
+
+document.getElementById('spaceForm').addEventListener('submit', function (event) {
+    event.preventDefault();
+
+    const escolhido = document.getElementById('escolher-user').value;
+    console.log('Usuário escolhido:', escolhido);
+});
+
+
+
+
 document.getElementById('spaceForm').addEventListener('submit', function (event) {
     event.preventDefault();
 
